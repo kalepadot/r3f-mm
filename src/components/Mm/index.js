fce0cf4be861462c22e630026146d07b6e125207
Jeremy Kale Padot <jeremy@padot.us>
2023-01-25 00:06:29 -0700

    new svg semi transparent, better positioning with camera

--- src/components/Mm/index.js
+++ src/components/Mm/index.js
@@ -1,14 +1,22 @@
-import React, { useRef, useState, Suspense } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef, useState, Suspense, useEffect } from 'react';
+import * as THREE from 'three';
+import { render } from 'react-dom';
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useGLTF } from '@react-three/drei';
-
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+extend({ EffectComposer, RenderPass, UnrealBloomPass });
 function Mm(props) {
-  const { nodes, materials } = useGLTF('/mmredglb.glb');
+  const ref = useRef();
+  const { nodes, materials } = useGLTF('/mmpink.glb');
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
 
   return (
     <group {...props} dispose={null}>
       <mesh
+        ref={ref}
         castShadow
         receiveShadow
         geometry={nodes.Curve.geometry}
@@ -17,19 +25,40 @@ function Mm(props) {
     </group>
   );
 }
+// function Bloom({ children }) {
+//   const { gl, camera, size } = useThree();
+//   const [scene, setScene] = useState();
+//   const composer = useRef();
+//   useEffect(
+//     () => void scene && composer.current.setSize(size.width, size.height),
+//     [size],
+//   );
+//   useFrame(() => scene && composer.current.render(), 1);
+//   return (
+//     <>
+//       <scene ref={setScene}>{children}</scene>
+//       <effectComposer ref={composer} args={[gl]}>
+//         <renderPass attachArray="passes" scene={scene} camera={camera} />
+//         <unrealBloomPass attachArray="passes" args={[undefined, 1.5, 1, 0]} />
+//       </effectComposer>
+//     </>
+//   );
+// }
 export default function App() {
   return (
     <Canvas
-      style={{ height: `340px` }}
+      style={{ height: `350px` }}
       shadows
-      camera={{ position: [0, 20, 4], fov: -10 }}
+      camera={{ position: [0, 60, 4], fov: -10 }}
     >
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <OrbitControls autoRotate />
+      <OrbitControls autoRotate autoRotateSpeed={4} />
       <Suspense fallback={null}>
+        {/* <Bloom> */}
         <Mm />
+        {/* </Bloom> */}
       </Suspense>
     </Canvas>
   );
